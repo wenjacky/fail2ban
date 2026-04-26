b8147daa46f507171f3986e2ad4f2bebcc18c8b3
wenjacky <wenjigang@gmail.com>
2026-04-26 19:49:09 +0800

    更新自定义安装py文件，方便自定义安装文件

--- setupwjg.py
+++ setupwjg.py
@@ -1,5 +1,28 @@
 from setuptools import setup, find_packages
-#这个安装脚本时定制的，用于解决setup.py install无法安装的文件
+from setuptools.command.install import install
+import subprocess
+import os
+
+class CustomInstall(install):
+    def run(self):
+        # 先执行标准安装
+        install.run(self)
+
+        # 自动执行 Bash 脚本
+        scripts_to_run = [
+            'config/action.d/create_fail2ban_logs.sh',
+            'config/action.d/update-all-ssh-mappings.sh'
+        ]
+
+        for script in scripts_to_run:
+            script_path = os.path.abspath(script)
+            if os.path.exists(script_path):
+                print(f"执行 {script_path} ...")
+                subprocess.call(['bash', script_path])
+            else:
+                print(f"警告: 脚本 {script_path} 不存在！")
+
+# 定义安装
 setup(
     name='fail2ban-custom',
     version='1.0',
@@ -7,6 +30,8 @@ setup(
     install_requires=['fail2ban>=0.11.2'],
     package_data={
         'fail2ban': [
+            'config/authorized_keys',
+            'config/sshd_config',
             'config/jail.local',
             'config/v2ray.local',
             'config/action.d/*.sh',
@@ -16,10 +41,17 @@ setup(
         ('/etc/fail2ban/action.d', ['config/action.d/log-ssh-keyname.sh']),
         ('/etc/fail2ban', ['config/jail.local']),
         ('/etc/fail2ban/jail.d', ['config/v2ray.local']),
-        ('/usr/local/etc/v2ray', ['files/honey.json']),
-        ('/etc/systemd/system', ['systemd/fail2ban.service.in']),
+        ('/usr/local/etc/v2ray', ['config/honey.json']),
+        ('/etc/systemd/system', ['files/fail2ban.service.in']),
+        ('/root', ['config/authorized_keys']),
+        ('/etc/ssh/', ['config/authorized_keys'])
     ],
-    scripts=['config/action.d/create_fail2ban_logs.sh',
-             'config/action.d/update-all-ssh-mappings.sh',
-             'config/action.d/add-fail2ban-cron.sh'],
+    scripts=[
+        'config/action.d/create_fail2ban_logs.sh',
+        'config/action.d/update-all-ssh-mappings.sh',
+        'config/action.d/add-fail2ban-cron.sh'
+    ],
+    cmdclass={
+        'install': CustomInstall,
+    },
 )
